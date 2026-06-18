b87e626524f0881fea99cc2f8cef49e27d98eba2
Daria Popova <88598902+DannyKim17@users.noreply.github.com>
2026-06-19 00:43:40 +0300

    Redesigning console menu for AE2 GUI

--- src/main/kotlin/Menu.kt
+++
@@ -1,7 +0,0 @@
-fun showCustomerMenu() {
-    println("=== Customer Menu ===")
-    println("1. View Films")
-    println("2. Book Ticket")
-    println("3. View Bookings")
-    println("0. Logout")
-}
