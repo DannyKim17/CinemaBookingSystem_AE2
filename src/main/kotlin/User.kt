eb175b7c053b103caf7a7490ef0f153fd50300b4
Daria Popova <88598902+DannyKim17@users.noreply.github.com>
2026-06-19 00:36:53 +0300

    Amending the code for AE2. More database friendly (will be mapped directly go SQL)

--- src/main/kotlin/User.kt
+++ src/main/kotlin/User.kt
@@ -1,5 +1,6 @@
-class User(
+data class User(
+    val id: Int = 0,
     val username: String,
     val password: String,
-    val role: String // Must be "Admin" or "Customer"
+    val role: String
 )
