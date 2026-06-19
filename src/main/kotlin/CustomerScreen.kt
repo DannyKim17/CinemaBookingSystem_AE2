a4bc9256ebeb1e3fda8789071ec0d25910e826cb
Daria Popova <88598902+DannyKim17@users.noreply.github.com>
2026-06-19 13:12:08 +0300

    Adding Customer menu for AE2 new structure

--- src/main/kotlin/CustomerScreen.kt
+++ src/main/kotlin/CustomerScreen.kt
@@ -1,2 +1,10 @@
 class CustomerScreen {
+
+    fun showMenu() {
+        println("=== Customer Menu ===")
+        println("1. View Films")
+        println("2. Book Ticket")
+        println("3. View Bookings")
+        println("0. Logout")
+    }
 }
