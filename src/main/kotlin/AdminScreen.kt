ec4af9b640ed8f897b668b521500bd79f2fb2065
Daria Popova <88598902+DannyKim17@users.noreply.github.com>
2026-06-19 12:36:13 +0300

    Redesigning admin menu for AE2

--- src/main/kotlin/AdminScreen.kt
+++ src/main/kotlin/AdminScreen.kt
@@ -1,2 +1,21 @@
 class AdminScreen {
+
+    fun viewFilmsAndScreenings(
+        films: List<Film>,
+        screenings: List<Screening>
+    ) {
+        println("\n===== Films and Screenings =====")
+
+        films.forEach { film ->
+            println("\n${film.title} (${film.genre}) - £${film.basePrice}")
+
+            screenings.filter { it.filmId == film.id }
+                .forEach {
+                    println(
+                        "Hall ${it.hallNumber} | " +
+                                "${it.date} | ${it.startTime}"
+                    )
+                }
+        }
+    }
 }
